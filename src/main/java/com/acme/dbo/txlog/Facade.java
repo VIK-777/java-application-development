a9d0c0c4a36558cad040196c8bc3d414d84358a0
Viktor Vishnevskiy <viktor332@gmail.com>
2020-03-10 11:04:52 +0300

    1st iteration

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,11 +1,35 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
+    private static void logToConsole(String message) {
+        System.out.println(message);
+    }
+
+    private static void logPrimitive(Object message) {
+        logToConsole("primitive: " + message);
+    }
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        logPrimitive(message);
+    }
+
+    public static void log(boolean message) {
+        logPrimitive(message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        logPrimitive(message);
+    }
+
+    public static void log(char message) {
+        logToConsole("char: " + message);
+    }
+
+    public static void log(String message) {
+        logToConsole("string: " + message);
+    }
+
+    public static void log(Object message) {
+        logToConsole("reference: " + message);
     }
 }
